349bbdcf2bd1e25489a980ba358ab47741c48a38
CarlesD <carles.dm@gmail.com>
2014-07-12 14:28:49 +0200

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -10,6 +10,7 @@ using namespace ofxCv;
 int serialPort;
 const char* portName="/dev/ttyACM3";
 
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 red = 100; blue = 200; green = 27;
