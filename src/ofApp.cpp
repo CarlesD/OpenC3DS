f956d248e0fcbadcf286adc98541e34c37719f0d
Carles <carles.dm@gmail.com>
2014-07-12 15:42:09 +0200

    ?

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -8,7 +8,7 @@
 using namespace cv;
 using namespace ofxCv;
 int serialPort;
-const char* portName="/dev/ttyACM31";
+const char* portName="/dev/ttyACM3";
 
 //--------------------------------------------------------------
 void ofApp::setup(){
