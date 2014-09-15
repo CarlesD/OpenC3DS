21510368c392c6471137371a0e8a7a3d591c05d2
acarreras <carreras.anna@gmail.com>
2014-09-15 19:03:28 +0200

    polifitgsl classes code formatting

--- src/polifitgsl.h
+++ src/polifitgsl.h
@@ -1,8 +1,11 @@
 #ifndef _POLIFITGSL_H
 #define _POLIFITGSL_H
+
 #include <gsl/gsl_multifit.h>
 #include <stdbool.h>
 #include <math.h>
-bool polynomialfit(int obs, int degree, 
-		   double *dx, double *dy, double *store); /* n, p */
+
+//COMENTARI Anna: polifitgsl ha de ser una classe?
+bool polynomialfit(int obs, int degree, double *dx, double *dy, double *store); /* n, p */
+
 #endif
